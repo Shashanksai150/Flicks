6aeb863ab4b4aae9c13fa8dbc95d5ed47064b5ca
manikanta2310 <manikantanaidu4880@gmail.com>
2022-03-10 17:01:54 +0530

    manikanta changes

---
+++ Flicks/CRexception/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRexception
+{
+    public class Class1
+    {
+    }
+}
