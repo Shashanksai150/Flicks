3701fad0d6e8c537c47ff64a68d23cf7df7aceed
GShark <saishashank150@gmail.com>
2022-03-10 16:59:54 +0530

    ProjectFile v2

--- Flicks/MovieEntity/Movie.cs
+++ Flicks/MovieEntity/Movie.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace MovieEntity
 {
-    public class Class1
+	/*[Movie ID] varchar(6) primary key not null,
+	Name varchar(30) not null,
+	[Language] varchar(10) not null,
+	Genres varchar(10),
+	Rating smallint ,
+	[Stock Available] smallint,
+	[Total Stock] smallint,
+	[BlueRay Price] money,
+	[Total Units rented] smallint,
+	[Revenue Generated] money*/
+    public class Movie
     {
+		public int MovieID { get; set; }
+		public string Title { get; set; }
+		public int Year { get; set; }
+		public string Language { get; set; }
+		public string Genres { get; set; }
+		public decimal Rating { get; set; }
+		public int StocKavailable { get; set; }
+		public int TotalStock { get; set; }
+		public int TotalUnitsrented { get; set; }
+		public double Bluerayprice { get; set; }
+		public double RevenueGenerated { get; set; }
+
+		
     }
 }
